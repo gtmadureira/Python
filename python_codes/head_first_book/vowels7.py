e69d8e29943dd6ff2c01c91bdbd143f9d5cfa658
Gustavo Madureira <gtmadureira@gmail.com>
2020-04-13 17:50:08 -0300

    Python Codes from Head First Book.

---
+++ python_codes/head_first_book/vowels7.py
@@ -0,0 +1,39 @@
+'''
+
+-----BEGIN PGP SIGNED MESSAGE-----
+Hash: SHA256
+
+'''
+
+
+
+vowels = set('aeiou')
+word = input('Provide a word to search for vowels: ')
+found = vowels.intersection(set(word))
+found = sorted(found)
+
+for vowel in found:
+
+  print(vowel)
+
+
+
+'''
+-----BEGIN PGP SIGNATURE-----
+
+iQIzBAEBCAAdFiEEE8iavjRZ2/dKhzGiBXQppWFastkFAl6Uz0QACgkQBXQppWFa
+stmJuA//TLz2AraZEEchD6N6moAWfwZHtwi4I2mTfb6d5dxJx+QlHA5lEaETb4T2
+7tO+Nj6/yRHIxQA9rEpRdrS8UZlag+z33FFszh30vvy9dbbS+bhwCRLMLfupt7b+
+2ytk7HmS22KQgxP7IxwKxt9PYckT+108CHR4onJk8YEYDGkBQK3+uGTCY6kZ5PWd
+a/nBUOvSxxxim/1zOu3BsNfrmqmTiNnNCzSQmDbNb1RVihJNRyCuuXuZuHrTgvXH
+D5FcxhkUJOlITHzy5+3MrgZ2nCg92KHn1oQjNt4LIuaDWNbKhrAYkzOkulNVhpRt
+qEZZR2UqcH2c0Ofsuh420bEGztr8h97NY1ZbmaSuHiT64hdrmfz7xJL5v/+Q4b5u
+VHh3lerFpO7RQEqtDlTfYnRXn57K7YWCFM1xJhQln2SyG6m7TpKsOzE2TPPCFehi
+vFgLEwzMdpprja1y5pW0zpSe8FCw3QTHwt5iIj+v+ONDQlJiLQXcpoz+iWWrUE7B
+WuhKbygn/0ATeXPsqTsfWo7QBI/iJvjbiaPFjCru/6GbbudYZGuSy96ct2hBsolM
+uxQiGWwZi4xg0xmwMkElyQr9SbAM/k4sW+YuZ+qR0ThB7zWP0dq9JF+q3URIwN6y
+ta+CgRsJbouzze1llQZr65t/0UyltzuGIYdb0ImMX36ERiRsq+Q=
+=kr/T
+-----END PGP SIGNATURE-----
+
+'''
