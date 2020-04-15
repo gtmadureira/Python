d3ab34702dda18d225a4214fd2a8dd466bb2e98c
Gustavo Madureira <gtmadureira@gmail.com>
2020-04-15 00:53:29 -0300

    Python Codes from Head First Book, (New files).

---
+++ python_codes/head_first_book/vsearch_test1.py
@@ -0,0 +1,41 @@
+"""
+
+-----BEGIN PGP SIGNED MESSAGE-----
+Hash: SHA256
+
+"""
+
+
+
+def search4vowels(word):
+
+    """ Display any vowels found in a supplied word. """
+
+    vowels = set('aeiou')
+    found = vowels.intersection(set(word))
+
+    for vowel in found:
+
+        print(vowel)
+
+
+
+"""
+-----BEGIN PGP SIGNATURE-----
+
+iQIzBAEBCAAdFiEEE8iavjRZ2/dKhzGiBXQppWFastkFAl6WanEACgkQBXQppWFa
+stnUag/5AUaCiv1PkfgFF+XgDVfa/MmiBV1DpbagKqTKGUi/AefdotRgEhbrFjH6
+XPK61IlwcCojxKaTf1Ihp3xtzJc0twXPjyYcBGiOcwFgUfZhiPaZdOkPcrp0QyIr
+rrIOsk4x88u12iFwERA7+gyabq4kHeSOAedssWgjA45oskvtT6K5gRGdVoLfWU6G
+/qU0EVL0ji91N4+IWa7TpBhUKnCPzyyBYbS16Z1vNtCRPHEdSmaeDJQLG9368tff
+V9sDyo0dZyXtyZC+wR/zgCSMAOXpzs9drI/eqlKrAP9tWQENVz/EGUZDjzjRWfjL
+TwmDf22Z84fBthk6xYkEGtpUbPeZVl/qSHLlyVGRy4+GfR/a2wFT/N21Tm6qTKho
+VkW5UWG1JGhBUF9wJCWR91SS8HSvnMZ0ctjsJ1N+82UlfaVgx1Xj0PdKqeZzmn5n
+G3Pxx08C6DMNokNaqxtyIzhCll9kxqe5v2RsTNWqAm8nAknH/EYoDHTV+f+99yAv
+4QJfrxaaw8h9KiiFhiuebzx6E7+bZfYHafnEGzm/8ar7cEFM8X5YsVPbcUD7nZIP
+eK1uWzI10Sm7flTeHmkNWLHmhSogdT2sfFKSl11dB+1s42074QylhRngeXeqvltv
+lRpXxIOS1FB0Fo86iaXgS7avb7tnaUOeoDQrLXapMKvWQ0ezDZk=
+=MiU4
+-----END PGP SIGNATURE-----
+
+"""
