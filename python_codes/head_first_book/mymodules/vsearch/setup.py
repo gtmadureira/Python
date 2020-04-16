8e034349a81388d9c9273f8054778ee6411cecc0
Gustavo Madureira <gtmadureira@gmail.com>
2020-04-16 17:28:37 -0300

    Python Codes from Head First Book, (Pack Changes).

--- python_codes/head_first_book/mymodules/vsearch/setup.py
+++ python_codes/head_first_book/mymodules/vsearch/setup.py
@@ -13,7 +13,7 @@ setup(
 
     name='vsearch',
     version='1.0',
-    description='The Head First Python Search Tools',
+    description='The Vowels/Letters Python Search Tools',
     author='Gustavo Madureira',
     author_email='gtmadureira@gmail.com',
     license='MIT',
@@ -28,19 +28,19 @@ setup(
 """
 -----BEGIN PGP SIGNATURE-----
 
-iQIzBAEBCAAdFiEEE8iavjRZ2/dKhzGiBXQppWFastkFAl6XrMgACgkQBXQppWFa
-stnJJxAAj8Gp7roLSlF7ZEghhVBJswkjzvI1Brlwk2tS+XXpD8f/AVydjbNrSNx6
-rFBPK9BZOQ6jvwFnvvqT0QALnB/HRkYkafG4tTrT50Z0PgV6wDZ/ZuKWpG9I64N+
-8anflu9yCafPY6lpEY4e/CRCRwgmrBY2vcpeWa4vrG3QCvS4ieIyze5y5MvsZb2E
-4sHdUKQiLodU+d5D9phI1R3p0bqcQ8r6JaYM5EP241a4dU7MUhOsrUjbB2vf6Mqg
-WPFQSW2x11C8DPIPhGXwcKvjJSoOdplgFzfkDHY2zlRfhYtCXzMkoBFKCfBjetAV
-ekYXybQEEaXXtsMl7IgWtP6p6yr92B3WSdIoHDZtWTjImay77hTQOmH4QpFIjZjy
-7ZcbnpLaiMLI65eWQES9EChj+a+jfu1T24TVK28yMhEulxqOGv3m1h6b3AJ7wNPm
-b1/yGpUtfPVGmhttPX+8hMcTHlc6//ElvWderMtf9d1VpQSq2aBN0cANHGZMYkeI
-CPT2kxSsk8kRNBmcG3gZDfWgg0At/ymrbzq/O8nD5yKtwAGN3RURvvULQHHDLuUT
-WX4OoB6s3pYSe7mua4Hjl4EU4W24InU8o1xpSSXn3LEqJmfmvYqX0QOnDQ67nrcB
-gBWehknh8bwKBppTnODZR77NPTnYupbkknEl8HOcCVPcIVbgsdI=
-=sjlo
+iQIzBAEBCAAdFiEEE8iavjRZ2/dKhzGiBXQppWFastkFAl6YvXAACgkQBXQppWFa
+stkXAg/+L7614CpUAmy66M1eYGYAq0RIQSsHhnqla/ke5ESSVXWIQsFNJiywqF6G
+Z8iOpFDuNvYf/aGybFW8I8Gdrt9f7rl5WidLgZbHPtJGIQJ6jnqGPSI0JN58/jTx
+rSr6RO9jvOzdloRlHTv937Kgyg6ld9T7gAjO6+RDHEaBo3V5YOVTZu4iFEPmMoS5
+AWUFYgNWrEI6Xc/Yl+XtNuSZxt44NhkF3z7VmGZGEL+/kfeSVLptH3zLEqU1BF4S
+I75vLYoYeLHV7FC7e3ZrJtcueZTgOG/y7w+Iw6XlTj5K1xRw8eqMbS31i+jZZJPL
+GNwNWZM4Kv4JMKBRC1eiqIEtkR6u1ziENR4DMbN7KxsQTEyqV/t+hYmVVmrBf8Oe
+FVv0wcNZfQJEDJOnXkviyYkhON6NbO4n8WqI5qC/HPlMMHxUvfF4DO4RsDg9yi1d
+qs8zTqAYX/egvBiTFVFssPGyJfs9lezl/yKuRW44plZs/wLc1260GCjlP/T9Q0h1
+HVH7xxkLl0DEFm35di6Zj17Akza2ErTSoFpOv0l+yWWZZ94bUytO5kv8aIdzRul5
+9QbrASfs8KSwWvipb1X/WOrFRfMtP4Ktin0ebeYGYHDj7k07MvhmqfkIcS9PWyIg
+lANWz2RurQxQ1m20lB5WNl9dGBt9X3x8wCLmhCFpQ5xmNqIp/6s=
+=mrJj
 -----END PGP SIGNATURE-----
 
 """
