c71861e3eba3a948ca76722f6637ca0efb2aabef
Gustavo Madureira <gtmadureira@gmail.com>
2020-04-15 22:02:30 -0300

    Python Codes from Head First Book, (New files and changes).

---
+++ python_codes/head_first_book/mymodules/vsearch/install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo -H python3.8 -m pip install ./dist/vsearch-1.0.tar.gz
+sudo -H python3.8 -m pip install ./dist/vsearch-1.0.tar.gz --upgrade
---
+++ python_codes/head_first_book/mymodules/vsearch/setup.py
@@ -0,0 +1,46 @@
+"""
+
+-----BEGIN PGP SIGNED MESSAGE-----
+Hash: SHA256
+
+"""
+
+
+
+from setuptools import setup
+
+setup(
+
+    name='vsearch',
+    version='1.0',
+    description='The Head First Python Search Tools',
+    author='Gustavo Madureira',
+    author_email='gtmadureira@gmail.com',
+    license='MIT',
+    url='https://gtmadureira.github.io/',
+    py_modules=['vsearch'],
+    platform='multiplataform'
+
+)
+
+
+
+"""
+-----BEGIN PGP SIGNATURE-----
+
+iQIzBAEBCAAdFiEEE8iavjRZ2/dKhzGiBXQppWFastkFAl6XrMgACgkQBXQppWFa
+stnJJxAAj8Gp7roLSlF7ZEghhVBJswkjzvI1Brlwk2tS+XXpD8f/AVydjbNrSNx6
+rFBPK9BZOQ6jvwFnvvqT0QALnB/HRkYkafG4tTrT50Z0PgV6wDZ/ZuKWpG9I64N+
+8anflu9yCafPY6lpEY4e/CRCRwgmrBY2vcpeWa4vrG3QCvS4ieIyze5y5MvsZb2E
+4sHdUKQiLodU+d5D9phI1R3p0bqcQ8r6JaYM5EP241a4dU7MUhOsrUjbB2vf6Mqg
+WPFQSW2x11C8DPIPhGXwcKvjJSoOdplgFzfkDHY2zlRfhYtCXzMkoBFKCfBjetAV
+ekYXybQEEaXXtsMl7IgWtP6p6yr92B3WSdIoHDZtWTjImay77hTQOmH4QpFIjZjy
+7ZcbnpLaiMLI65eWQES9EChj+a+jfu1T24TVK28yMhEulxqOGv3m1h6b3AJ7wNPm
+b1/yGpUtfPVGmhttPX+8hMcTHlc6//ElvWderMtf9d1VpQSq2aBN0cANHGZMYkeI
+CPT2kxSsk8kRNBmcG3gZDfWgg0At/ymrbzq/O8nD5yKtwAGN3RURvvULQHHDLuUT
+WX4OoB6s3pYSe7mua4Hjl4EU4W24InU8o1xpSSXn3LEqJmfmvYqX0QOnDQ67nrcB
+gBWehknh8bwKBppTnODZR77NPTnYupbkknEl8HOcCVPcIVbgsdI=
+=sjlo
+-----END PGP SIGNATURE-----
+
+"""
