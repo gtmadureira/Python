071d3182b838b3f1cf2952098992c0e05c3b34a7
Gustavo Madureira <gtmadureira@gmail.com>
2020-04-17 22:18:27 -0300

    Python Codes from Head First Book.

--- python_codes/head_first_book/mymodules/vsearch/vsearch.py
+++ python_codes/head_first_book/mymodules/vsearch/vsearch.py
@@ -7,7 +7,7 @@ Hash: SHA256
 
 
 
-def search4vowels(phrase:str) -> set:
+def search4vowels(phrase: str) -> set:
 
     """ Return any vowels found in a supplied phrase. """
 
@@ -16,7 +16,7 @@ def search4vowels(phrase:str) -> set:
     return vowels.intersection(set(phrase))
 
 
-def search4letters(phrase:str, letters:str='aeiou') -> set:
+def search4letters(phrase: str, letters: str='aeiou') -> set:
 
     """ Return a set of the 'letters' found in phrase. """
 
@@ -27,19 +27,19 @@ def search4letters(phrase:str, letters:str='aeiou') -> set:
 """
 -----BEGIN PGP SIGNATURE-----
 
-iQIzBAEBCAAdFiEEE8iavjRZ2/dKhzGiBXQppWFastkFAl6WhIsACgkQBXQppWFa
-stmQFg/6An8MCcSneA7hNV7SVxoYZZqN4uzmd4c+lqKy5hK01yq+h3yACDeA9DZF
-D/8p5DRtvu/T0zMV1frcPo+3r3bBa1rmXHy4g/wddXc+Tawves43ttI5XN7sVEH3
-1Ns2c48etvF5hfkP1F0wkhp4EyDMFkdWHJmJieAccekM/hH5C7dKMJ/3GSre4JW2
-Yk8+5YypL7OiPekhqlf1Y8gbmtIzZBsxjnsxIaSUQWixQa31NVLBSQKoGwpedTfq
-3t+PJIP+tziF7H5Kgh7QpoQDzdX3UkZ6sTbo+5sAW8i6v1OhGAxg/zRK64dPGnSg
-CKX9ItrfIOsEgpxorCM5z4aZouOntLgSSBvGSn6bRjkn5n5Inhio1qhDNZr8cifq
-1omUFSZqZ/E7UwrYNAeAh5sFPcZXshU/kavc0yZDE2aFOdSmyeIhCfGN5jjDyDWt
-8LoP73pKTG5EECkH/BHb8DA5H/gPzHwpYIgHdOiFDhAj7N+ac7QkdOXyjAv/eTWs
-TIcLE9qEI/6NYWLJIsWBXflTkIW7vqon0ls44+UPe+DCXDmKalN0Cc1rnfTXAXsK
-X7QNwLvCJtNQZF8S07IY0ZzU1zA6DKhNPbZjM1HpMC/0zFj0xngIS6R0KVb4ZIUe
-lMGa+PpGSbszKt4db7GE65AY1CSIOKYyACGd59b+Zlzgmpl6XOk=
-=WOHs
+iQIzBAEBCAAdFiEEE8iavjRZ2/dKhzGiBXQppWFastkFAl6aVSYACgkQBXQppWFa
+stnTXg/+NgeHQSpqjLAZynZ3jJGB3Ok5XHx4AC98Q/rv8myfpfK9UG0zOsbLBVLR
+r7a56dtHdj1plS98+4NlgifZUhos5kPGTW8XioUWF4UP8ZFMBStjYqIin2aCYi+r
+EAsX9x0JflT23xm3GFwNRVnnOCxrtqlRwJrXHSgF8bB2ztYw4DAjNfXC/K3hmaN2
+qivqvYC9K3XUvqhfMqAULPU2vumbvnmmkUguUiirzx+1etsvVOsRKoMHUg4aVXGW
+JOF8XqzwlMKzMHYWZ4li2D21meuHXz1M0vFVd+M6SZYaH6w0P4Hi745ZdTS5t4uz
+m8jUpHzptu9f8dz266ktuZbVfO+ndXuYaJXuSXvfqhfJSUIUf50lN4taDQoiguUy
+jGpbEzVGyIg5lHtgSUvPWaONO6QcvR3aj83+asm6T8fWBjfaeD5uErpG7DEoIVz5
+k9GEnJal9RiEkWMln75BI5VrlOYyF0MiJO1cLkUdjlcQylhtzAHPMOSNiEhJvYXl
+akf1SoL3rhCB2anloKF8zCnmQ+3MV/YRblJ1N6vmhQ1rUBrOMHY+mxhqvbSEcADE
+WtRQXZi6OLBOiy97jigTVK9+3FXv8inH9oI7u+RPz2nzaw3RKyGKSfL/wjASf12+
+3ibMO5NuneiTmLIihTh7kAp3O6PvEn1Yi/B1LcTJ+nyGq8EexA8=
+=WPMN
 -----END PGP SIGNATURE-----
 
 """
