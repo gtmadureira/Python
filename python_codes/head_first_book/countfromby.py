537763d8ea738565f6e38a96d9ea265d2d5b0d6c
Gustavo Madureira <gtmadureira@gmail.com>
2020-04-23 23:59:21 -0300

    Python Codes from Head First Book (chapter 8 - classes).

---
+++ python_codes/head_first_book/countfromby.py
@@ -0,0 +1,35 @@
+"""
+
+-----BEGIN PGP SIGNED MESSAGE-----
+Hash: SHA256
+
+"""
+
+
+
+class CountFromBy:
+
+    def increase(self) -> None:
+        self.val += self.incr
+
+""" I stopped on page n-322 """
+
+"""
+-----BEGIN PGP SIGNATURE-----
+
+iQIzBAEBCAAdFiEEE8iavjRZ2/dKhzGiBXQppWFastkFAl6iVRwACgkQBXQppWFa
+stmiSw/8C6HwN7W1ebU7WKfcSjTDO6nJD6zuE+Faq0O5RbH3aMkJUWIVqoK1ntOt
+tKJJdJCxeQwi+2iNcB/WCZcwcuwUvoghb+zmCsbpNk19K0DAw16ABsZr2Pu/x1wM
+aLQzzSbmGPkqDItyyz/V5q2YbfIC8aT3CLGawiq0eFO+ayfGvUuTjNnsnQ+RHRz2
++m+CiwzvRFvIvkAf/6hh2PaZhDmcJebscOHWd9s1lOuw9z56kwHiCHBadkcHv6Zy
+Q9xVZ8YMlLS4C+Lctdj+4l/NHDKdTpRjk+TCStuvwozfbSCFmblUdbF+T3g8WuVd
+WxsiCNhPUIWIjttlCdOBnOwMSezX5R6cX65vdhT2AJW84F9vLjOkuhpA1VV/Zlcl
+3B2hlPTfgd4k7r/sGXwTP6ZQbw8Fdm2gUPe6qVogvMCgX5vXmyHLK8zb1cfb3OoN
+39fm8caPhHT8ryyJlmuYvuSLMZUn21B2X7dgJ4GHKuIgUlMo5lh6mB83fBIZtiZ3
+OtHB94b8vFE8vLxDPOjCzQCf6/e/ofRp6b2Esjif6u2//kC0NVQlhqBnoK9TYTh6
+j2GiDmTRqHwbscV/N3RM0XLEimRNpWDtPLzqx3LezesQMROO5e9VTm8RLTt2c6II
+0bbvdSk4ZymVCsjLauspjBsbjReoEHP3F5mDoDINuJG3qCxvsfE=
+=Q/LT
+-----END PGP SIGNATURE-----
+
+"""
