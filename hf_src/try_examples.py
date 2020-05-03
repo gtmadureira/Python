b875a57feb1c0b2b0548842482e8346abe94bcd1
Gustavo Madureira <gtmadureira@gmail.com>
2020-05-02 23:27:02 -0300

    Head First - Python
    Back to web app.

--- hf_src/try_examples.py
+++ hf_src/try_examples.py
@@ -6,5 +6,5 @@ except FileNotFoundError:
     print('The data file is missing.')
 except PermissionError:
     print('This is not allowed.')
-except:
-    print('Some other error occured.')
+except Exception as err:
+    print('Some other error occured: ', str(err))
