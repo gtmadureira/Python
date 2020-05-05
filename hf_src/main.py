dc1982c84298bd46f52538fb142af3b919d4de61
Gustavo Madureira <gtmadureira@gmail.com>
2020-05-04 23:45:51 -0300

    New gitiginore configuration.
    Removed ass files *.pyc .

--- hf_src/main.py
+++ hf_src/main.py
@@ -4,4 +4,4 @@ def soma(x: float, y: float) -> float:
 
 if __name__ == "__main__":
 
-    print(soma(10, 10))
+    print(soma(15, 30))
--- tests/testsoma.py
+++ tests/testsoma.py
@@ -3,5 +3,5 @@ from hf_src.main import soma
 
 
 class TestSoma(unittest.TestCase):
-    def test_retorno_soma_10_10(self):
-        self.assertEqual(soma(10, 10), 20)
+    def test_retorno_soma_15_30(self):
+        self.assertEqual(soma(15, 30), 45)
